c57b9fc17f7d4f41147b0ffa12d15d5194625ad1
REDMOND\mergreen <mergreen@microsoft.com>
2026-07-17 15:02:22 -0700

    adding guidance for user to run the cler set up script

--- Lib/RDO_ML/KMeansRDO.cpp
+++ Lib/RDO_ML/KMeansRDO.cpp
@@ -105,7 +105,8 @@ std::vector<std::vector<uint8_t>> KMeansRDO::ClusterRDOWithLoss(
     if (needsModels && !hasModels)
     {
         Utility::Printf(GACL_Logging_Priority_Medium, L"Warning: loss metric %s requires models, but no models provided; using MSE loss instead.\n", LossMetrics::ToString(lossMetric).c_str());
-        lossMetric = LossMetrics::Metric::MSE;
+        Utility::Printf(GACL_Logging_Priority_Medium, L"To enable moded-based perceptual loss metrics, ensure you have run the setup script: Tools\\scripts\\setupCLER.ps1\n");
+        lossMetric = LossMetrics::Metric::MSE;  
     }
 
     const bool useOrtTensors = LossMetrics::requiresOnnx(lossMetric);
